42c4fd336120a1485461a2a8908b04885e9d8733
Brett Gleason <bmgdx8@mail.missouri.edu>
2018-07-28 15:28:02 -0500

    feat: Confirm valid triangle given
    Add function to confirm that side lengths given by the user form a valid triangle.

--- Triangle.c
+++ Triangle.c
@@ -4,6 +4,8 @@ struct triangle_3s {
 	int a, b, c;
 };
 
+int verify_triangle(struct triangle_3s);
+
 int main(void) {
 	printf("Please enter the lengths of the triangle's sides:\n");
 	printf("(Answer given in same units as side lengths, all lengths must be in same units.)\n");
@@ -17,7 +19,27 @@ int main(void) {
 
 	struct triangle_3s t1 = {a, b, c};
 
-	printf("Length of side a: %d, side b: %d, side c: %d\n", t1.a, t1.b, t1.c);
+	int tri_check = verify_triangle(t1);
+
+	if (tri_check == 1) {
+		printf("Triangle is valid.\n");
+	}
+	else if (tri_check == 0) {
+		printf("Invalid triangle.\n");
+	}
 
 	return 0;
+}
+
+int verify_triangle(struct triangle_3s triangle) {
+	/* Return 1 if triangle is valid, else return 0 */
+	/* Use Triangle Inequality Theorem to make sure triangle is valid. */
+	if (triangle.a + triangle.b > triangle.c &&
+			triangle.a + triangle.c > triangle.b &&
+			triangle.b + triangle.c > triangle.a) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
 }
