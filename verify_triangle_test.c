62882bae7c931305d156b74c0fc52e403c2c4f0c
Brett Gleason <bmgdx8@mail.missouri.edu>
2018-08-03 08:04:47 -0500

    test: Add test cases for the verify_triangle() function
    Test with largest possible valid triangle as well as invalid triangles
    with integer overflow.

--- verify_triangle_test.c
+++ verify_triangle_test.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <limits.h>
 #include "Triangle.h"
+
+#define TEST_COUNT 10 // Increment TEST_COUNT each time a test case is added
 
 int main(void) {
 	/* Assumptions:
 	 *  - At this point in the code the input has been validated. The
-	 *    triangle side lengths are non-negative integers that are smaller
+	 *    triangle side lengths are positive, nonzero integers that are smaller
 	 *    than the upper limit of an unsigned 32 bit integer. */
 
-	/* TODO: test function for case where triangle side lengths are
-	 * individually smaller than the 32 bit integer limit but add up to be
-	 * larger */
-
-	/* Increment test_count each time a test case is added */
-	const uint32_t test_count = 7;
 
 	/* If I try to declare the test array using test_count as the size, I
 	 * get a compilation error because the size of the array is not known
@@ -22,14 +19,18 @@ int main(void) {
 	 * compile time error. I'd like to figure out a way to initialize these
 	 * values that is more similar to the commented lines so that the test
 	 * case reasoning can be described. */
-	struct triangle_3s test[7] = {  {3, 4, 5},
-		                        {1000, 1000, 1000},
-				        {200, 200, 390},
-					{50, 60, 70},
-					{100, 50, 30},
-					{50, 100, 30},
-					{50, 30, 100} };
-	uint32_t exp_result[test_count]; // 0 for invalid triangle, 1 for valid triangle
+	struct triangle_3s test[TEST_COUNT] = { {3, 4, 5},
+		                        	{1000, 1000, 1000},
+				        	{200, 200, 390},
+						{50, 60, 70},
+						{100, 50, 30},
+						{50, 100, 30},
+						{50, 30, 100},
+						{UINT_MAX, UINT_MAX, UINT_MAX},
+						{UINT_MAX, 10000, 10000},
+						{UINT_MAX, UINT_MAX, 50000}, };
+
+	uint32_t exp_result[TEST_COUNT]; // 0 for invalid triangle, 1 for valid triangle
 
 	// test[0] = {3, 4, 5}; // Valid triangle, right triangle
 	exp_result[0] = 1;
@@ -45,12 +46,18 @@ int main(void) {
 	exp_result[5] = 0;
 	// test[6] = {50, 30, 100}; // Invalid, side C too long
 	exp_result[6] = 0;
+	// test[7] = {UINT_MAX, UINT_MAX, UINT_MAX}; // Largest valid triangle
+	exp_result[7] = 1;
+	// test[8] = {UINT_MAX, 10000, 10000}; // Invalid triangle with integer overflow
+	exp_result[8] = 0;
+	// test[9] = {UINT_MAX, UINT_MAX, 50000}; // Invalid triangle with integer overflow
+	exp_result[9] = 0;
 
 	
 
 	uint32_t result = 2;
 	
-	for (int i = 0; i < (sizeof(test) / sizeof(struct triangle_3s)); i++) {
+	for (uint32_t i = 0; i < (sizeof(test) / sizeof(struct triangle_3s)); i++) {
 		printf("Test case %u: {%u, %u, %u}\n", i + 1, test[i].a, test[i].b, test[i].c);
 		result = verify_triangle(test[i]);
 		if (result == 1) {
